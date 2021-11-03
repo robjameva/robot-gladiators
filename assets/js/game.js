d1f40fefff7e5c2a9da5b1ece2aecb1785271c20
Robert Evanik <rob.evanik@gmail.com>
2021-11-02 22:44:32 -0400

    3.2.6 added multiple robots

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,12 +3,12 @@ var playerHealth = 100;
 var playerAtack = 10;
 var playerMoney = 10;
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
 
-var fight = function () {
+var fight = function (enemyName) {
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
 
@@ -68,4 +68,6 @@ var fight = function () {
     }
 };
 
-fight();
+for (var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
